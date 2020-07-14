d6b129f1100ccecb732e5345d71b40a26fe10cc8
Haeuncs <haeun.developer@gmail.com>
2020-07-15 01:28:06 +0900

    Set Target 11.0

---
+++ VocaDesignSystem/VocaDesignSystem/UIColor+.swift
@@ -0,0 +1,13 @@
+//
+//  UIColor+.swift
+//  VocaDesignSystem
+//
+//  Created by LEE HAEUN on 2020/07/15.
+//  Copyright © 2020 LEE HAEUN. All rights reserved.
+//
+
+import UIKit.UIColor
+
+extension UIColor {
+  
+}
---
+++ VocaDesignSystem/VocaDesignSystem/VocaDesignSystem.h
@@ -0,0 +1,19 @@
+//
+//  VocaDesignSystem.h
+//  VocaDesignSystem
+//
+//  Created by LEE HAEUN on 2020/07/15.
+//  Copyright © 2020 LEE HAEUN. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VocaDesignSystem.
+FOUNDATION_EXPORT double VocaDesignSystemVersionNumber;
+
+//! Project version string for VocaDesignSystem.
+FOUNDATION_EXPORT const unsigned char VocaDesignSystemVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VocaDesignSystem/PublicHeader.h>
+
+
