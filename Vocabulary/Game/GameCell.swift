efee4bec83aa925ad48e4e67cf463d021a0d31df
Haeuncs <haeun.developer@gmail.com>
2020-08-02 00:00:25 +0900

    [PreDev] GameViewController

---
+++ Vocabulary/Game/GameCell.swift
@@ -0,0 +1,73 @@
+//
+//  GameCell.swift
+//  Vocabulary
+//
+//  Created by user on 2020/08/01.
+//  Copyright © 2020 LEE HAEUN. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class GameCell: UITableViewCell {
+
+    static let reuseIdentifier = String(describing: GameCell.self)
+
+    lazy var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.backgroundColor = .white
+        view.layer.cornerRadius = 16
+        return view
+    }()
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    lazy var arrowButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .black
+        return button
+    }()
+
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureLayout()
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    func configure(title: String) {
+        titleLabel.text = title
+    }
+
+    func configureLayout() {
+        contentView.addSubview(containerView)
+        containerView.addSubview(titleLabel)
+        containerView.addSubview(arrowButton)
+
+        containerView.snp.makeConstraints { (make) in
+            make.top.bottom.equalTo(contentView)
+            make.leading.equalTo(contentView).offset(16)
+            make.trailing.equalTo(contentView).offset(-16)
+        }
+
+        titleLabel.snp.makeConstraints { (make) in
+            make.top.equalTo(containerView).offset(50)
+            make.bottom.equalTo(containerView).offset(-50)
+            make.centerX.equalTo(containerView)
+        }
+
+        arrowButton.snp.makeConstraints { (make) in
+            make.centerY.equalTo(containerView)
+            make.trailing.equalTo(containerView).offset(-20)
+            make.height.width.equalTo(24)
+        }
+    }
+}
---
+++ Vocabulary/Game/GameViewController.swift
@@ -0,0 +1,91 @@
+//
+//  GameViewController.swift
+//  Vocabulary
+//
+//  Created by user on 2020/07/31.
+//  Copyright © 2020 LEE HAEUN. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class GameViewController: UIViewController {
+
+    enum GameType: String {
+        case flip = "뒤집기"
+        case matching = "매칭 게임"
+    }
+
+    enum Constant {
+        static let gameList: [GameType] = [.flip, .matching]
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .gray
+        configureLayout()
+    }
+
+    func configureLayout() {
+        view.addSubview(titleLabel)
+        view.addSubview(gameListTableView)
+
+        titleLabel.snp.makeConstraints { (make) in
+            make.centerX.equalTo(view)
+            make.top.equalTo(view).offset(89)
+        }
+
+        gameListTableView.snp.makeConstraints { (make) in
+            make.top.equalTo(titleLabel.snp.bottom)
+            make.leading.trailing.equalTo(view)
+            make.bottom.equalTo(view.safeAreaLayoutGuide)
+        }
+    }
+
+    lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.text = "복습 해볼까?"
+        return label
+    }()
+
+    lazy var containerView: UIView = {
+        let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+
+    lazy var gameListTableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(
+            GameCell.self,
+            forCellReuseIdentifier: GameCell.reuseIdentifier
+        )
+        tableView.rowHeight = UITableView.automaticDimension
+        tableView.delegate = self
+        tableView.dataSource = self
+        tableView.backgroundColor = .gray
+        return tableView
+    }()
+}
+
+extension GameViewController: UITableViewDelegate {
+    
+}
+
+extension GameViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Constant.gameList.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: GameCell.reuseIdentifier, for: indexPath) as? GameCell else {
+            return UITableViewCell()
+        }
+        cell.configure(title: Constant.gameList[indexPath.row].rawValue)
+        return cell
+    }
+
+
+}
