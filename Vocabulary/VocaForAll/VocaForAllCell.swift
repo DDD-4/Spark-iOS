db11cf99afc67a24a4cbd7f598547605e21fd234
hyeonjae Lee <odyflame@icloud.com>
2020-08-27 21:10:06 +0900

    Update VocaForAllCell UI Refactoring UI
    update VacaForAllCell ui with latest wireframe design.

--- Vocabulary/VocaForAll/VocaForAllCell.swift
+++ Vocabulary/VocaForAll/VocaForAllCell.swift
@@ -17,65 +17,71 @@ class VocaForAllCell: UITableViewCell {
     static let reuseIdentifier = String(describing: VocaForAllCell.self)
     
     enum Constant {
-        static let sideMargin: CGFloat = 16
         enum Image {
-            static let height: CGFloat = 263
+            static let height: CGFloat = 166
         }
-        enum TextContents {
-            static let contentTopMargin: CGFloat = 60
-            static let height: CGFloat = 343
-            static let radius: CGFloat = 32
-        }
-        enum Edit {
-            static let width: CGFloat = 18
-        }
-        static let micImage: UIImage? = UIImage(named: "icVoice")
-        static let micImageHeight: CGFloat = 48
         static let imageRadius: CGFloat = 12
-
     }
     
     lazy var vocaImageView: UIImageView = {
         let view = UIImageView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.contentMode = .scaleAspectFill
+        view.contentMode = .scaleAspectFit
         view.clipsToBounds = true
-        view.layer.cornerRadius = 12
+        view.layer.cornerRadius = Constant.Image.height * 0.5
+        view.backgroundColor = .lightGray
         return view
     }()
-    lazy var numberView: UILabel = {
+    lazy var numberView: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true
+        view.layer.cornerRadius = 7
+        view.layer.borderWidth = 1.7
+        view.layer.borderColor = UIColor.white.cgColor
+        view.backgroundColor = .orange
+        return view
+    }()
+    lazy var numberLable: UILabel = {
         let view = UILabel()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.layer.cornerRadius = 12
-        view.backgroundColor = .gray
+        view.clipsToBounds = true
+        view.layoutMargins = UIEdgeInsets(top: 4, left: 4, bottom: 4, right: 4)
         return view
-    }()
-    lazy var stackView: UIStackView = {
-        let stack = UIStackView(arrangedSubviews: [titleLabel, authorLabel])
-        stack.translatesAutoresizingMaskIntoConstraints = false
-        stack.axis = .vertical
-        stack.alignment = .fill
-        stack.distribution = .fillEqually
-        stack.spacing = 3
-        return stack
     }()
     lazy var titleLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.textColor = .black
+        label.font = .BalsamiqSansBold(size: 30)
+        label.numberOfLines = 0
+        label.textColor = .darkIndigo
         return label
+    }()
+    lazy var authorContentView: UIView = {
+       let view = UIView()
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
     }()
     lazy var authorLabel: UILabel = {
         let label = UILabel()
         label.translatesAutoresizingMaskIntoConstraints = false
-        label.numberOfLines = 1
+        label.numberOfLines = 0
         label.textColor = .gray
         return label
+    }()
+    lazy var authorImageView: UIImageView = {
+       let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.image = UIImage(named: "icPicture")
+        imageView.contentMode = .scaleAspectFit
+        imageView.clipsToBounds = true
+        imageView.layer.cornerRadius = Constant.imageRadius
+        return imageView
     }()
     lazy var baseView: UIView = {
         let view = UIView()
         view.translatesAutoresizingMaskIntoConstraints = false
-        view.layer.cornerRadius = 12
+        view.layer.cornerRadius = 32
         view.backgroundColor = .white
         return view
     }()
@@ -90,39 +96,77 @@ class VocaForAllCell: UITableViewCell {
     }
 
     func configure(dummy: VocaForAll) {
-        titleLabel.text = dummy.title
+        //titleLabel.text = dummy.title
+        titleLabel.text = "내가 좋아하는 달달한 간식"
+        authorLabel.text = "홍길동"
+        if let urlImage = URL(string: dummy.words[0].imageURL) {
+            vocaImageView.sd_setImage(with: urlImage)
+        }
+        authorImageView.image = UIImage(named: "icPicture")
+        numberLable.text = "\(dummy.words.count)"
     }
 
     func configure(group: Group) {
         titleLabel.text = group.title
         authorLabel.text = "\(group.identifier)"
-        vocaImageView.image = UIImage(named: "icPicture")
-        numberView.text = "\(group.words.count)"
+        if let data = group.words[0].image {
+            vocaImageView.image = UIImage(data: data)
+        }
+        authorImageView.image = UIImage(named: "icPicture")
+        numberLable.text = "\(group.words.count)"
     }
     
     func configureLayout() {
-        
+        contentView.backgroundColor = .none
         contentView.addSubview(baseView)
-        baseView.addSubview(titleLabel)
         baseView.addSubview(vocaImageView)
-        //baseView.addSubview(numberView)
+        baseView.addSubview(numberView)
+        baseView.addSubview(titleLabel)
+        baseView.addSubview(authorContentView)
+        baseView.addSubview(numberView)
+        numberView.addSubview(numberLable)
+        authorContentView.addSubview(authorImageView)
+        authorContentView.addSubview(authorLabel)
         
         baseView.snp.makeConstraints { (make) in
             make.top.bottom.equalTo(contentView)
             make.leading.equalTo(contentView)
             make.trailing.equalTo(contentView)
-        }
-        
-        vocaImageView.snp.makeConstraints { (make) in
-            make.trailing.equalTo(baseView).offset(-12)
-            make.top.equalTo(baseView).offset(12)
-            make.bottom.equalTo(baseView).offset(-12)
-            make.height.width.equalTo(96)
+            make.width.height.equalTo(343)
         }
         
         titleLabel.snp.makeConstraints { (make) in
-            make.leading.equalTo(baseView).offset(24)
-            make.centerY.equalTo(baseView)
+            make.leading.equalTo(baseView.snp.leading).offset(32)
+            make.top.equalTo(baseView.snp.top).offset(32)
+            make.width.equalTo(196)
+        }
+        
+        authorContentView.snp.makeConstraints { (make) in
+            make.height.equalTo(24)
+            make.leading.equalTo(baseView.snp.leading).offset(5)
+            make.top.equalTo(titleLabel.snp.bottom).offset(10)
+        }
+        authorImageView.snp.makeConstraints { (make) in
+            make.leading.top.bottom.equalTo(authorContentView)
+        }
+        authorLabel.snp.makeConstraints { (make) in
+            make.leading.equalTo(authorImageView.snp.trailing).offset(-16)
+            make.centerY.equalTo(authorImageView)
+        }
+        
+        vocaImageView.snp.makeConstraints { (make) in
+            make.height.width.equalTo(Constant.Image.height)
+            make.bottom.equalTo(baseView.snp.bottom).offset(-32)
+            make.trailing.equalTo(baseView.snp.trailing).offset(-32)
+        }
+        
+        numberView.snp.makeConstraints { (make) in
+            make.centerY.equalTo(vocaImageView).offset(-20)
+            make.trailing.equalTo(vocaImageView.snp.trailing)
+        }
+        
+        numberLable.snp.makeConstraints { (make) in
+            make.leading.top.trailing.bottom.equalTo(numberView)
         }
         
     }
